d7589739b78b064a0394eddedc0f0a208dd13455
hero010809 <289280282@qq.com>
2018-09-14 01:14:12 +0800

    去除日志

--- business/mine_business.py
+++ business/mine_business.py
@@ -1,9 +1,7 @@
 from handle.mine_handle import MineHandle
 from util.get_by_local import GetByLocal
 from base.base_driver import BaseDriver
-from log import my_log
-
-logger = my_log.logger
+from log import print_log
 
 
 class MineBusiness:
@@ -18,17 +16,12 @@ class MineBusiness:
 
     def go_mine_pass(self):
         """进入我的页面正确"""
-        try:
-            self.mine_handle.click_mine_tab_element()
-            self.mine_handle.click_user_image_element()
-            # if self.mine_page.get_sign_button_element() and self.mine_page.get_user_image_element():
-            #     return True
-            # else:
-            #     return False
-        except Exception as e:
-            logger.error(e)
-        finally:
-            log.close_handle()
+        self.mine_handle.click_mine_tab_element()
+        self.mine_handle.click_user_image_element()
+        # if self.mine_page.get_sign_button_element() and self.mine_page.get_user_image_element():
+        #     return True
+        # else:
+        #     return False
 
     def get_driver(self):
         driver = self.driver
