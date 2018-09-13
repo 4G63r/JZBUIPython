c2a8b4cc516ef2b22084632537dfc5f984647a0c
hero010809 <289280282@qq.com>
2018-09-14 01:15:14 +0800

    代码格式化

--- case/test_case.py
+++ case/test_case.py
@@ -4,13 +4,10 @@ import HTMLTestRunner
 import HTMLTestRunnerNew
 import multiprocessing
 import time
-
-
 from util.write_user_command import WriteUserCommand
-from business.search_business import SearchBusiness
 from business.mine_business import MineBusiness
 from util.server import Server
-from log import my_log
+from log.print_log import Log
 
 
 class ParameTestCase(unittest.TestCase):
@@ -19,64 +16,41 @@ class ParameTestCase(unittest.TestCase):
         global parames
         parames = parame
 
+
 class CaseTest(ParameTestCase):
     @classmethod
     def setUpClass(cls):
         print("setupclass----->", parames)
-        # cls.search_business = SearchBusiness(i)
         cls.mine_business = MineBusiness(i)
         cls.driver = cls.mine_business.get_driver()
 
+    @classmethod
+    def tearDownClass(cls):
+        print('this is class teardown')
+        # cls.driver.quit()
+
     def setUp(self):
-        print("this is setup")
-
-    def test_01(self):
-        '''成功进入搜索页面'''
-        flag = self.mine_business.go_mine_pass()
-        try:
-            self.assertTrue(flag, msg="进入搜索页面测试不通过")
-        except :
-
-            my_log.logger("")
-
-
-    def test_02(self):
-        '''搜索功能模块的用例2'''
-        self.search_business.search_pass()
-        self.search_business.back()
-
-    def test_03(self):
-        '''搜索功能模块的用例3'''
-        self.search_business.search_pass()
-        self.search_business.back()
-
-    def test_04(self):
-        '''搜索功能模块的用例4'''
-        self.search_business.search_pass()
-        self.search_business.back()
-
-    def test_05(self):
-        '''搜索功能模块的用例5'''
-        self.search_business.search_pass()
-        self.search_business.back()
-
-    # @unittest.skip('CaseTest')
-    # def test_02(self):
-    #     print('this is case2')
+        self.logger = Log()
+        self.logger.info("############################### START ###############################")
 
     def tearDown(self):
-        print('this is teardown')
+        self.logger.info('###############################  End  ###############################')
         for method_name, error in self._outcome.errors:
             if error:
                 case_name = self._testMethodName
                 file_name = os.getcwd().replace('case', 'result/screenshot/') + case_name + '.png'
                 self.driver.save_screenshot(file_name)
 
-    @classmethod
-    def tearDownClass(cls):
-        cls.log.close_handle()
-        print('this is class teardown')
-        # cls.driver.quit()
+    def test_01(self):
+        '''成功进入搜索页面'''
+        flag = self.mine_business.go_mine_pass()
+        self.assertTrue(flag, msg="进入搜索页面测试不通过")
+
+
+    def test_02(self):
+        '''搜索功能模块的用例2'''
+        self.search_business.search_pass()
+        self.search_business.back()
 
 
 def appium_init():
