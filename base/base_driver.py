dd4a8c65cf55d794d3dab87ee4c980f4e7755640
songxiao <songxiao@100tal.com>
2018-09-14 22:37:54 +0800

    添加必要的异常捕获和日志打印

--- base/base_driver.py
+++ base/base_driver.py
@@ -1,16 +1,21 @@
 from appium import webdriver
 from util.write_user_command import WriteUserCommand
 import time
+from log.print_log import Log
+
+logger = Log()
+success = "SUCCESS   "
+fail = "FAIL   "
 
 
 class BaseDriver:
     def android_driver(self, i):
         # devices_name adb devices
         # port
-        print("this is android_driver:", i)
         write_file = WriteUserCommand()
         device = write_file.get_value('user_info_' + str(i), 'deviceName')
         port = write_file.get_value('user_info_' + str(i), 'port')
+        start_t = time.time()
         capabilities = {
             "platformName": "Android",
             # "automationName": "UiAutomator2",
@@ -20,9 +25,15 @@ class BaseDriver:
             "unicodeKeyboard": "true",
             "resetKeyboard": "true"
         }
-        driver = webdriver.Remote('http://localhost:' + port + '/wd/hub', capabilities)
-        time.sleep(10)
-        return driver
+        try:
+            driver = webdriver.Remote('http://localhost:' + port + '/wd/hub', capabilities)
+            logger.info(
+                "{0} Start Android device <{1}>, Spend {2} seconds".format(success, device, time.time() - start_t))
+            time.sleep(10)
+            return driver
+        except Exception:
+            logger.info(
+                "{0} Start Android device <{1}>, Spend {2} seconds".format(fail, device, time.time() - start_t))
 
     def ios_driver(self):
         pass
