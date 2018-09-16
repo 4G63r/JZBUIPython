f34ce5bb7d1111fbd1741e4791a5cb1e85d650ae
songxiao <songxiao@100tal.com>
2018-09-17 00:13:57 +0800

    perfect testcase

--- business/mine_business.py
+++ business/mine_business.py
@@ -1,27 +1,71 @@
+from util.element_action import ElementAction
+from page.mine_page import MinePage
 from handle.mine_handle import MineHandle
-from util.get_by_local import GetByLocal
-from base.base_driver import BaseDriver
-from log import print_log
+from page.login_page import LoginPage
+from handle.login_handle import LoginHandle
+from log.print_log import Log
+
+logger = Log()
+success = "SUCCESS   "
+fail = "FAIL   "
 
 
 class MineBusiness:
     """我的模块业务交互"""
 
-    def __init__(self, i):
-        base_driver = BaseDriver()
-        self.driver = base_driver.android_driver(i)
-        self.get_by_local = GetByLocal(self.driver)
-        self.mine_handle = MineHandle(self.driver)
-        # self.mine_page = MinePage(self.driver)
+    def __init__(self, driver):
+        self.mp = MinePage(driver)
+        self.mh = MineHandle(driver)
+        self.lp = LoginPage(driver)
+        self.lh = LoginHandle(driver)
+        self.ea = ElementAction(driver)
+
+    def check_user_isTourist(self):
+        """检查用户身份"""
+        if self.mh.get_user_name() == '游客':
+            return True
+        else:
+            return False
 
     def go_mine_pass(self):
-        """进入我的页面正确"""
-        self.mine_handle.click_mine_tab_element()
-        self.mine_handle.click_user_image_element()
-        # if self.mine_page.get_sign_button_element() and self.mine_page.get_user_image_element():
-        #     return True
-        # else:
-        #     return False
+        """成功进入我的页面&验证"""
+        self.mh.click_mine_tab_element()
+        if self.ea.element_exist(self.mp.get_user_image_element()) and self.ea.element_exist(
+                self.mp.get_sign_btn_element()):
+            return True
+        else:
+            return False
 
-    def get_driver(self):
-        driver = self.driver
+    def login_pass(self):
+        """成功登录
+        1、以游客判断登录状态，如果是游客身份
+        2、点击我的头像，进入手机号登录页面
+        3、输入手机号，点击发送验证码
+        4、输入验证码页面输入验证码点击登录
+        5、再次以是否为游客判断是否登录成功
+        """
+        if self.mh.get_user_name == '游客':
+            self.mh.click_user_image_element()
+
+    def test_loginRegister_panel(self):
+        """测试未登录时的登录注册弹框"""
+        self.ea.swipe_on('up')
+        flag = False
+        el_lists = [
+            self.mp.get_friend_btn_element(), self.mp.get_collect_btn_element(),
+            self.mp.get_sign_btn_element(), self.mp.get_childInfo_btn_element(),
+            self.mp.get_showcaseView_btn_element(), self.mp.get_courseOrder_btn_element()
+        ]
+        if self.check_user_isTourist():
+            for i in el_lists:
+                self.ea.click(i)
+                if self.ea.element_exist(self.lp.get_login_panel_element()) == True:
+                    flag = True
+                    self.lh.click_close_btn()
+                else:
+                    flag = False
+                    break
+            return flag
+
+    def aa(self):
+        self.mh.click_user_image_element()
